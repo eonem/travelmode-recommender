644a22833cfb077dc3700095d8e87bc6f85f99c6
Evren Onem <evren.onem@synchronoss.com>
2017-05-14 20:55:04 +0100

    Dockerization of the app: individual Docker files and a docker-compose yml.

--- travel-mode-recommender-service/src/main/java/travelmode/recommender/Application.java
+++ travel-mode-recommender-service/src/main/java/travelmode/recommender/Application.java
@@ -1,12 +1,15 @@
 package travelmode.recommender;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableCircuitBreaker
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     @Bean
